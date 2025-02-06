1bdf8eb7563eec832d80db30af7b7e6d77232ec1
rfights <159859811+rfights@users.noreply.github.com>
2025-02-06 14:44:56 +0000

    undid that because I did it incorrectly

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -3,10 +3,9 @@ using FactorialLibrary;
 public class FactorialServiceTests
 {
   [Fact]
-public void Test_negative()
-    {
-    Assert.Throws<InvalidOperationException>(()=>{
-        var result = FactorialService.Factorial(-1);
-    });
-    }
+  public void Test1()
+  {
+    var result = FactorialService.Factorial(0);
+    Assert.Equal(1,result);
+  }
 }
--- FactorialLibrary/FactorialService.cs
+++ FactorialLibrary/FactorialService.cs
@@ -1,10 +1,9 @@
 ﻿namespace FactorialLibrary;
+
 public class FactorialService
+
 {
   public static int Factorial(int n) {
-    if(n<0) {
-      throw new InvalidOperationException("Negative numbers don't have proper factorial");
-    }
     if(n==1 || n==0) {
       return 1;
     }
