f726e232f3155a13645ef4fe73b66a0116d06d34
rfights <159859811+rfights@users.noreply.github.com>
2025-02-06 14:43:58 +0000

    updated files

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -3,9 +3,10 @@ using FactorialLibrary;
 public class FactorialServiceTests
 {
   [Fact]
-  public void Test1()
-  {
-    var result = FactorialService.Factorial(0);
-    Assert.Equal(1,result);
-  }
+public void Test_negative()
+    {
+    Assert.Throws<InvalidOperationException>(()=>{
+        var result = FactorialService.Factorial(-1);
+    });
+    }
 }
